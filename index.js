25bd81f612a93ed8fcac0ca7572b2c0faf5a43b4
Emma Repp <emmarepp.draws@gmail.com>
2022-08-17 13:13:07 -1000

    put titles into array and created list of shows

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
+
+
+
+function getShows(){
+   // const ul = document.getElementById('list')
+
+fetch("http://localhost:3000/shows")
+  .then(res => res.json())
+  .then(data => {
+
+  const showArray = data.map(a => a.title);
+
+  
+  let list = document.getElementById("list");
+  
+  showArray.forEach((item)=>{
+    let li = document.createElement("li");
+    li.innerText = item;
+    list.appendChild(li);
+  })
+
+   
+  }) }
+
+ 
+
+
+document.addEventListener('DOMContentLoaded', function() {
+   getShows(); 
+
+   
+
+})
+
+
+
+
+    // data is an array of object, 
+    // iterate over it and create an in-memory CSV
+    // or verify if Plotly.d3 can be fed directly this array
+
+
 // code here
+
